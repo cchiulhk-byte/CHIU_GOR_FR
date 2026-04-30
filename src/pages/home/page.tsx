7643a13f61ea1a5269e38925520c3bb1b31dff7f
Chi Chiu Lam <chichiulam@Chi-Chius-MacBook-Pro.local>
2026-04-30 13:25:44 +0800

    change page order

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -23,8 +23,8 @@ export default function Home() {
         <HeroSection />
         <WhyChooseMeSection />
         <AboutSection />
-        <FeaturedBlogCarousel />
         <ExperienceSection />
+        <FeaturedBlogCarousel />
         <CoursesSection />
         <TestimonialsSection />
         <ContactSection />
