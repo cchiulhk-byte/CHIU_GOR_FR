23495ee1ec71fa20f6552d8ef093cc5474461d41
Chi Chiu Lam <chichiulam@Chi-Chius-MacBook-Pro.local>
2026-05-15 01:44:32 +0800

    navbar color change

--- src/pages/home/components/Navbar.tsx
+++ src/pages/home/components/Navbar.tsx
@@ -152,7 +152,7 @@ export default function Navbar({ isDark, onToggleDark }: NavbarProps) {
                 </button>
               )}
               {i < 4 && i < navLinks.length - 1 && (
-                <span className="w-px h-4" style={{ backgroundColor: '#fcbf49' }} />
+                <span className="w-px h-4" style={{ backgroundColor: '#fbaf16' }} />
               )}
             </span>
           ))}
